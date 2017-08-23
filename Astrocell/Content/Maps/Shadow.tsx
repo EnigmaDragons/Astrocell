183541f9fa9feabf326fee3c23eb6277015c4976
noah reinagel <noah.r9@gmail.com>
2017-08-22 22:18:59 -0700

    Add fire cave

---
+++ Astrocell/Content/Maps/Shadow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ShadowTile" tilewidth="48" tileheight="48" tilecount="1" columns="1">
+ <image source="ShadowTile.png" width="48" height="48"/>
+</tileset>
