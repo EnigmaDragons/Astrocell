79fea372c3f49f1af2b3517129442a473a18c0d0
Silas Reinagel <silas.reinagel@gmail.com>
2017-10-02 19:40:35 -0700

    Fixed file location of MonoDragons.TiledEditor

---
+++ Astrocell/MonoDragons.TiledEditor - Copy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+using MonoDragons.Core.Common;
+using MonoDragons.Core.Engine;
+using MonoDragons.Core.Inputs;
+using MonoDragons.Core.Navigation;
+using MonoDragons.Core.Render;
+using MonoDragons.Core.Scenes;
+
+namespace Astrocell
+{
+#if WINDOWS || LINUX
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using (var game = new NeedlesslyComplexMainGame(
+                "Astrocell",
+                "Large",
+                new Display(1600, 900, false, 1),
+                CreateSceneFactory(),
+                CreateController()))
+            {
+                game.Run();
+            }
+        }
+
+        private static IController CreateController()
+        {
+            return new KeyboardController(new Map<Keys, Control>
+            {
+                { Keys.Z, Control.A },
+            });
+        }
+
+        private static SceneFactory CreateSceneFactory()
+        {
+            return new SceneFactory(new Dictionary<string, Func<IScene>>
+            {
+            });
+        }
+    }
+#endif
+}
---
+++ MonoDragons.TiledEditor/Scenes/MapSelector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using MonoDragons.Core.Common;
+using MonoDragons.Core.Entities;
+using MonoDragons.Core.Navigation;
+using MonoDragons.Core.PhysicsEngine;
+using MonoDragons.Core.Scenes;
+using MonoDragons.Core.UserInterface;
+
+namespace MonoDragons.TiledEditor.Scenes
+{
+    public class MapSelector : EcsScene
+    {
+        protected override IEnumerable<GameObject> CreateObjs()
+        {
+            yield return OptionPicker.Create("Pick Map", 
+                new Transform2
+                {
+                    Size = new Size2(120, 40),
+                    Center = new Vector2(800, 30)
+                }, 
+                Directory.GetFiles(Path.Combine("Content", "Maps"))
+                    .Where(fileName => Path.GetExtension(fileName).ToLower() == ".tmx")
+                    .Select(mapName => new Option(Path.GetFileName(mapName), () => Navigate.To(new MapEditor(GetRelativePathUpOneFolder(mapName))))).ToArray()); 
+        }
+
+        private string GetRelativePathUpOneFolder(string path)
+        {
+            var newPath = path.Substring(path.IndexOf('\\') + 1);
+            return newPath;
+        }
+    }
+}
