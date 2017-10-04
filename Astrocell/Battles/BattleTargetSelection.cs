c01356fb724b69c4bfb961f02c946ab60aba7d2a
Silas Reinagel <silas.reinagel@gmail.com>
2017-10-03 23:46:50 -0700

    Player does not have to selected Card Effect Targets for inferred targets. Closes #128

--- Astrocell/Battles/BattleTargetSelection.cs
+++ Astrocell/Battles/BattleTargetSelection.cs
@@ -3,26 +3,33 @@ using Astrocell.Battles.Decks;
 using MonoDragons.Core.Common;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Astrocell.Battles
 {
     public sealed class BattleTargetSelection
     {
+        private Optional<Card> SelectedCard { get; set; } = Optional<Card>.Missing;
         private BattleCharacters Chars { get; set; }
         private BattleCharacter Source { get; set; }
-        private Optional<Card> SelectedCard { get; set; } = Optional<Card>.Missing;
         private Action<CardAction> Callback { get; set; }
 
         private List<TargettedEffect> ChosenEffects { get; set; } = new List<TargettedEffect>();
         private int _effectIndex;
+
+        private CardEffect CurrentEffect => SelectedCard.Value.Effects[_effectIndex];
 
         public void OnCharacterIndicated(BattleCharacter character)
         {
             if (!SelectedCard.IsPresent)
                 return;
 
-            ChosenEffects.Add(new TargettedEffect(SelectedCard.Value.Effects[_effectIndex], character.AsList()));
-            if (++_effectIndex < SelectedCard.Value.Effects.Count)
+            ChosenEffects.Add(new TargettedEffect(CurrentEffect, character.AsList()));
+
+            while (++_effectIndex < SelectedCard.Value.Effects.Count && RequiresNoManualTargetting(CurrentEffect))
+                ChosenEffects.Add(SelectInferredTargets(Source, Chars, CurrentEffect));
+
+            if (_effectIndex < SelectedCard.Value.Effects.Count)
                 return;
 
             var card = SelectedCard.Value;
@@ -32,17 +39,33 @@ namespace Astrocell.Battles
 
         public void OnCardSelected(BattleCharacter src, BattleCharacters chars, Card card, Action<CardAction> onActionSelected)
         {
-            if (card.Effects.None())
+            if (card.Effects.None() || card.Effects.TrueForAll(RequiresNoManualTargetting))
             {
-                onActionSelected(new CardAction { Card = card, Source = src, TargettedEffects = new List<TargettedEffect>() });
+                var effects = card.Effects.Select(x => SelectInferredTargets(src, chars, x)).ToList();
+                onActionSelected(new CardAction { Card = card, Source = src, TargettedEffects = effects });
                 return;
             }
-
+            
             Callback = onActionSelected;
             SelectedCard = card;
             Source = src;
             _effectIndex = 0;
             ChosenEffects.Clear();
+        }
+
+        private bool RequiredManualTargetting(CardEffect effect)
+        {
+            return effect.Target == EffectTarget.One;
+        }
+
+        private bool RequiresNoManualTargetting(CardEffect effect)
+        {
+            return effect.Target != EffectTarget.One;
+        }
+
+        private TargettedEffect SelectInferredTargets(BattleCharacter src, BattleCharacters chars, CardEffect effect)
+        {
+            return new TargettedEffect(effect, chars.GetPossibleTargets(src, effect.Target));
         }
     }
 }
